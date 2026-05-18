4d63e48aa2467fbf62352a1749a711f4e873ed8f
Naya <nayadhet.cosme@gmail.com>
2026-05-18 02:34:18 -0400

    naya cosas
    animación de correr zorro
    corrupcion pocha (fuego estirado xd)
    mas mapa
    parallax maoma
    MENU ARREGLADO (faltan que los botones funken)

--- Assets/Scripts/ParallaxLayer.cs
+++ Assets/Scripts/ParallaxLayer.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class ParallaxLayer : MonoBehaviour
 {
     [SerializeField] private Transform cam;
+
     [Range(0f, 1f)]
     [SerializeField] private float parallaxFactorX = 0.2f;
 
     private float startX;
     private float startCamX;
+
+    private float spriteWidth;
 
     void Start()
     {
@@ -16,15 +19,33 @@ public class ParallaxLayer : MonoBehaviour
 
         startX = transform.position.x;
         startCamX = cam.position.x;
+
+        spriteWidth =
+            GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     void LateUpdate()
     {
         if (cam == null) return;
 
-        float camDeltaX = cam.position.x - startCamX;
-        float newX = startX + camDeltaX * parallaxFactorX;
+        float camDeltaX =
+            cam.position.x - startCamX;
 
-        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        float newX =
+            startX + camDeltaX * parallaxFactorX;
+
+        transform.position = new Vector3(
+            newX,
+            transform.position.y,
+            transform.position.z
+        );
+
+        float distanceFromCam =
+            cam.position.x - transform.position.x;
+
+        if (distanceFromCam >= spriteWidth)
+        {
+            startX += spriteWidth * 2f;
+        }
     }
 }
